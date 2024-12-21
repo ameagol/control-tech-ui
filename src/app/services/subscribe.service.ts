c4ae3cb32c1f8d8f3a9d6c7acc4afbc7a6d80a15
Ricardo Gellman <ricardo.gellman@dev.pro>
2024-12-21 11:20:35 -0300

    fix app

--- src/app/services/subscribe.service.ts
+++ src/app/services/subscribe.service.ts
@@ -3,7 +3,6 @@ import {HttpClient } from '@angular/common/http';
 import {Observable } from 'rxjs';
 import {API, PROD_HOST} from "../constants/device-options.constants";
 import {Subscribe} from "../model/subscribe.model";
-import {User} from "../model/user.model";
 
 @Injectable({
     providedIn: 'root'
@@ -13,7 +12,7 @@ export class SubscribeService {
 
     constructor(private http: HttpClient) {}
 
-    newSubscribe(subscribe: Subscribe): Observable<User> {
-        return this.http.post(`${this.apiUrl}`, subscribe);
+    newSubscribe(subscribe: Subscribe): Observable<Subscribe> {
+        return this.http.post<Subscribe>(`${this.apiUrl}`, subscribe);
     }
 }
